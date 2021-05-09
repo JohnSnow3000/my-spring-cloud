489f7db3ade108644d9b079bcf32e6fcff32106f
xw.lihongchao <lihongchao@microwu.com>
2021-05-09 23:40:18 +0800

    feat(module):add eureka-server,eureka-client

---
+++ eureka-server/src/main/java/com/hongchao/eureka/domain/Person.java
@@ -0,0 +1,15 @@
+package com.hongchao.eureka.domain;
+
+//import lombok.Data;
+//import lombok.experimental.Accessors;
+//
+//@Data
+//@Accessors(chain = true)
+public class Person {
+
+    private String name;
+
+    private Integer age;
+
+
+}
